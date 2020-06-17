36dad5d72993be49f60a1dde3774a57391487b75
XGDavid <39276408+XGDavid@users.noreply.github.com>
2020-06-17 22:27:48 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,112 @@
+# XGTZonesGUI
+
+## Description
+
+
+This is a PocketMine Plugin.
+Open a GUI and choose where you want to teleport.
+
+This plugin is in BETA!
+
+## How to install
+
+1. Install plugin
+
+2. Stop/start server
+
+3. Edit config
+
+4. Restart server
+
+5. Have FUN!
+
+## Use
+
+This plugin use Vector3 and InvMenu by muqsit!
+
+
+## Features
+
+- [X] Config
+
+- [X] Costom Message
+
+- [X] Custom Item Name
+
+- [X] Custom Type
+
+
+
+## Other
+
+[![Discord](https://img.shields.io/discord/689211475537297411?logo=discord)](https://discord.gg/h8uTKFh)
+
+[![ForTheBadge built-with-love](http://ForTheBadge.com/images/badges/built-with-love.svg)](https://github.com/XGDavid)
+
+
+## Config
+
+```
+# Type:
+ # 1 = This type has 4 slots that you can select
+ # 2 = This type has 7 slots that you can select
+
+# Name:
+# Here you put a custom name for an item
+# (optional)Use for Color = §
+Name-Chest: "Crates"
+Name-Melon: "Farm"
+Name-GoldenApple: "Spawn"
+Name-Sword: "PVP"
+Name-Egg: "Food Shop"
+Name-Stone: "Mine"
+Name-Sand: "Shop"
+
+# Message:
+# Here you put a personalized message when someone clicks on that item
+# (optional)Use for Color = §
+Message-Chest: "Teleport to Crate Zone!"
+Message-Melon: "Teleport to Farm Zone!"
+Message-GoldenApple: "Teleport to Spawn Zone!"
+Message-Sword: "Teleport to PVP Zone!"
+Message-Egg: "Teleport to Food shop Zone!"
+Message-Stone: "Teleport to Mine Zone!"
+Message-Sand: "Teleport to Shop Zone!"
+
+# Coordinates:
+# Here you put the coordinates where Jews teleport you when you click on an item
+  #Golden Apple
+    GAplle-X: 104
+    GAplle-Y: 80
+    GAplle-Z: 40
+
+  #Chest
+    Chest-X: 104
+    Chest-Y: 80
+    Chest-Z: 40
+
+  #Melon
+    Melon-X: 90
+    Melon-Y: 80
+    Melon-Z: 40
+
+  #Sowrd
+    DMDSword-X: 104
+    DMDSword-Y: 80
+    DMDSword-Z: 40
+
+  #Egg
+    EGG-X: 104
+    EGG-Y: 80
+    EGG-Z: 40
+
+  #Stone
+    Stone-X: 104
+    Stone-Y: 80
+    Stone-Z: 40
+
+  #Sand
+    Sand-X: 104
+    Sand-Y: 80
+    Sand-Z: 40
+```
---
+++ src/XGDAVIDYT/XGTZonesGUI/Main.php
@@ -0,0 +1,179 @@
+<?php
+
+declare(strict_types=1);
+
+namespace XGDAVIDYT\XGTZonesGUI;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat;
+use pocketmine\event\entity;
+use pocketmine\event\Listener;
+use pocketmine\utils\Config;
+use pocketmine\utils\Utils;
+use pocketmine\plugin\Plugin;
+use pocketmine\plugin\PluginManager;
+use pocketmine\math\Vector3;
+
+use muqsit\invmenu\{InvMenu, InvMenuHandler, MenuIds, InvMenuEventHandler};
+use pocketmine\inventory\transaction\action\SlotChangeAction;
+
+class Main extends PluginBase implements Listener{
+
+	public function onEnable() : void{
+		$this->saveResource("config.yml");
+		if(!$this->config->get("version") == "1.0.0-beta1"){
+			$this->getServer()->getLogger()->notice("[XGTZonesGUI] Config is outdata!");
+			$this->getServer()->getPluginManager()->disablePlugin($this);
+		}
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
+
+        $chest = $this->getConfig()->get("Name-Chest");
+		$gapple = $this->getConfig()->get("Name-GoldenAplle");
+		$melon = $this->getConfig()->get("Name-Melon");
+		$sword = $this->getConfig()->get("Name-Sword");
+		$egg = $this->getConfig()->get("Name-Egg");
+		$stone = $this->getConfig()->get("Name-Stone");
+		$sand = $this->getConfig()->get("Name-Sand");
+		$megg = $this->getConfig()->get("Message-Egg");
+		$mchest = $this->getConfig()->get("Message-Chest");
+		$mgapple = $this->getConfig()->get("Message-GoldenAplle");
+		$mmelon = $this->getConfig()->get("Message-Melon");
+		$msword = $this->getConfig()->get("Message-Sword");
+		$mstone = $this->getConfig()->get("Message-Stone");
+		$msand = $this->getConfig()->get("Message-Sand");
+		$locked = $this->getConfig()->get("Name-Locked");
+
+		if($command->getName() === "zones"){
+			if($sender instanceof Player){
+				$menu = InvMenu::create(InvMenu::TYPE_CHEST);
+				$inventory = $menu->getInventory();
+				if($this->getConfig()->get("Type") == 1){
+					$menu->setListener(function(Player $player, Item $itemClicked, Item $itemClickedWith, SlotChangeAction $action) : void{
+					if($itemClicked->getId() === ItemIds::GOLDEN_APPLE){
+						$player->removeWindow($action->getInventory());
+						$player->sendMessage("§8[§bXGTZonesGUI§8]§lZonesGUI§8]§l»§r§7 ".$mgapple);
+						$player->teleport(new Vector3($this->getConfig()->get("GAplle-X"), $this->getConfig()->get("GAplle-Y"), $this->getConfig()->get("GAplle-Z")));
+					}
+					if($itemClicked->getId() === ItemIds::CHEST){
+						$player->removeWindow($action->getInventory());
+						$player->sendMessage("§8[§bXGTZonesGUI§8]§lZonesGUI§8]§l»§r§7 ".$mchest);
+						$player->teleport(new Vector3($this->getConfig()->get("Chest-X"), $this->getConfig()->get("Chest-Y"), $this->getConfig()->get("Chest-Z")));
+					}
+					if($itemClicked->getId() === ItemIds::MELON){
+						$player->removeWindow($action->getInventory());
+						$player->sendMessage("§8[§bXGTZonesGUI§8]§lZonesGUI§8]§l»§r§7 ".$mmelon);
+						$player->teleport(new Vector3($this->getConfig()->get("Melon-X"), $this->getConfig()->get("Melon-Y"), $this->getConfig()->get("Melon-Z")));
+					}
+					if($itemClicked->getId() === ItemIds::DIAMOND_SWORD){
+						$player->removeWindow($action->getInventory());
+						$player->sendMessage("§8[§bXGTZonesGUI§8]§lZonesGUI§8]§l»§r§7 ".$msword);
+						$player->teleport(new Vector3($this->getConfig()->get("DMDSword-X"), $this->getConfig()->get("DMDSword-Y"), $this->getConfig()->get("DMDSword-Z")));
+					}
+					});
+					$block = Item::get(Block::IRON_BARS);
+					$menu->getInventory()->setItem(10, Item::get(Item::GOLDEN_APPLE)->setCustomName($gapple));
+					$menu->getInventory()->setItem(12, Item::get(Item::CHEST)->setCustomName($chest));
+					$menu->getInventory()->setItem(14, Item::get(Item::DIAMOND_SWORD)->setCustomName($sword));
+					$menu->getInventory()->setItem(16, Item::get(Item::MELON)->setCustomName($melon));
+					$menu->getInventory()->setItem(1, $block);
+					$menu->getInventory()->setItem(2, $block);
+					$menu->getInventory()->setItem(3, $block);
+					$menu->getInventory()->setItem(4, $block);
+					$menu->getInventory()->setItem(5, $block);
+					$menu->getInventory()->setItem(6, $block);
+					$menu->getInventory()->setItem(7, $block);
+					$menu->getInventory()->setItem(8, $block);
+					$menu->getInventory()->setItem(9, $block);
+					$menu->getInventory()->setItem(17, $block);
+					$menu->getInventory()->setItem(18, $block);
+					$menu->getInventory()->setItem(19, $block);
+					$menu->getInventory()->setItem(20, $block);
+					$menu->getInventory()->setItem(21, $block);
+					$menu->getInventory()->setItem(22, $block);
+					$menu->getInventory()->setItem(23, $block);
+					$menu->getInventory()->setItem(24, $block);
+					$menu->getInventory()->setItem(25, $block);
+					$menu->getInventory()->setItem(0, $block);
+					$menu->getInventory()->setItem(26, $block);
+				}elseif($this->getConfig()->get("Type") == 2){
+					$menu->setListener(function(Player $player, Item $itemClicked, Item $itemClickedWith, SlotChangeAction $action) : void{
+						if($itemClicked->getId() === ItemIds::GOLDEN_APPLE){
+							$player->removeWindow($action->getInventory());
+							$player->sendMessage("§8[§bXGTZonesGUI§8]§lZonesGUI§8]§l»§r§7 ".$mgapple);
+							$player->teleport(new Vector3($this->getConfig()->get("GAplle-X"), $this->getConfig()->get("GAplle-Y"), $this->getConfig()->get("GAplle-Z")));
+						}
+						if($itemClicked->getId() === ItemIds::CHEST){
+							$player->removeWindow($action->getInventory());
+							$player->sendMessage("§8[§bXGTZonesGUI§8]§lZonesGUI§8]§l»§r§7 ".$mchest);
+							$player->teleport(new Vector3($this->getConfig()->get("Chest-X"), $this->getConfig()->get("Chest-Y"), $this->getConfig()->get("Chest-Z")));
+						}
+						if($itemClicked->getId() === ItemIds::MELON){
+							$player->removeWindow($action->getInventory());
+							$player->sendMessage("§8[§bXGTZonesGUI§8]§lZonesGUI§8]§l»§r§7 ".$mmelon);
+							$player->teleport(new Vector3($this->getConfig()->get("Melon-X"), $this->getConfig()->get("Melon-Y"), $this->getConfig()->get("Melon-Z")));
+						}
+						if($itemClicked->getId() === ItemIds::DIAMOND_SWORD){
+							$player->removeWindow($action->getInventory());
+							$player->sendMessage("§8[§bXGTZonesGUI§8]§lZonesGUI§8]§l»§r§7 ".$mgapple);
+							$player->teleport(new Vector3($this->getConfig()->get("DMDSword-X"), $this->getConfig()->get("DMDSword-Y"), $this->getConfig()->get("DMDSword-Z")));
+						}
+						if($itemClicked->getId() === ItemIds::STONE){
+							$player->removeWindow($action->getInventory());
+							$player->sendMessage("§8[§bXGTZonesGUI§8]§lZonesGUI§8]§l»§r§7 ".$mstone);
+							$player->teleport(new Vector3($this->getConfig()->get("Stone-X"), $this->getConfig()->get("Stone-Y"), $this->getConfig()->get("Stone-Z")));
+						}
+						if($itemClicked->getId() === ItemIds::EGG){
+							$player->removeWindow($action->getInventory());
+							$player->sendMessage("§8[§bXGTZonesGUI§8]§lZonesGUI§8]§l»§r§7 ".$megg);
+							$player->teleport(new Vector3($this->getConfig()->get("EGG-X"), $this->getConfig()->get("EGG-Y"), $this->getConfig()->get("EGG-Z")));
+						}
+						if($itemClicked->getId() === ItemIds::SAND){
+							$player->removeWindow($action->getInventory());
+							$player->sendMessage("§8[§bXGTZonesGUI§8]§lZonesGUI§8]§l»§r§7 ".$msand);
+							$player->teleport(new Vector3($this->getConfig()->get("Sand-X"), $this->getConfig()->get("Sand-Y"), $this->getConfig()->get("Sand-Z")));
+						}
+						});
+					$block = Item::get(Block::IRON_BARS)->setCustomName($locked);
+					$menu->getInventory()->setItem(10, Item::get(Item::GOLDEN_APPLE)->setCustomName($gapple));
+					$menu->getInventory()->setItem(11, Item::get(Item::CHEST)->setCustomName($chest));
+					$menu->getInventory()->setItem(12, Item::get(Item::DIAMOND_SWORD)->setCustomName($sword));
+					$menu->getInventory()->setItem(13, Item::get(Item::MELON)->setCustomName($melon));
+					$menu->getInventory()->setItem(14, Item::get(Item::EGG)->setCustomName($egg));
+					$menu->getInventory()->setItem(15, Item::get(Item::STONE)->setCustomName($stone));
+					$menu->getInventory()->setItem(16, Item::get(Item::SAND)->setCustomName($sand));
+					$menu->getInventory()->setItem(1, $block);
+					$menu->getInventory()->setItem(2, $block);
+					$menu->getInventory()->setItem(3, $block);
+					$menu->getInventory()->setItem(4, $block);
+					$menu->getInventory()->setItem(5, $block);
+					$menu->getInventory()->setItem(6, $block);
+					$menu->getInventory()->setItem(7, $block);
+					$menu->getInventory()->setItem(8, $block);
+					$menu->getInventory()->setItem(9, $block);
+					$menu->getInventory()->setItem(17, $block);
+					$menu->getInventory()->setItem(18, $block);
+					$menu->getInventory()->setItem(19, $block);
+					$menu->getInventory()->setItem(20, $block);
+					$menu->getInventory()->setItem(21, $block);
+					$menu->getInventory()->setItem(22, $block);
+					$menu->getInventory()->setItem(23, $block);
+					$menu->getInventory()->setItem(24, $block);
+					$menu->getInventory()->setItem(25, $block);
+					$menu->getInventory()->setItem(0, $block);
+					$menu->getInventory()->setItem(26, $block);
+				}
+				$menu->readonly();
+				$menu->setName("XGT Zones's");
+				$menu->send($player);
+			}elseif($sender instanceof Player){
+				$sender->sendMessage("[XGTZonesGUI] > Command only In-Game!");
+			}
+		}
+        return true;
+	}
+}
