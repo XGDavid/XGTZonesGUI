b1951a0dbda9025a2019b20687ab7bfd989cea9b
XGDavid <39276408+XGDavid@users.noreply.github.com>
2020-06-18 00:20:16 +0300

    Add files via upload

--- src/XGDAVIDYT/XGTZonesGUI/Main.php
+++ src/XGDAVIDYT/XGTZonesGUI/Main.php
@@ -66,7 +66,7 @@ class Main extends PluginBase implements Listener{
 					$menu->setListener(function(Player $player, Item $itemClicked, Item $itemClickedWith, SlotChangeAction $action) : void{
 					if($itemClicked->getId() === ItemIds::GOLDEN_APPLE){
 						$player->removeWindow($action->getInventory());
-						$player->sendMessage("§8[§bXGTZonesGUI§8]§lZonesGUI§8]§l »§r§7 ".$this->config->get("Message-GoldenAplle"););
+						$player->sendMessage("§8[§bXGTZonesGUI§8]§lZonesGUI§8]§l »§r§7 ".$this->config->get("Message-GoldenAplle"));
 						$player->teleport(new Vector3($this->config->get("GAplle-X"), $this->config->get("GAplle-Y"), $this->config->get("GAplle-Z")));
 					}
 					if($itemClicked->getId() === ItemIds::CHEST){
